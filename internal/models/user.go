bc946936c094cfad93c272c6c8fc98e73df06d7d
ZmaximillianZ <goooglemax1993@gmail.com>
2021-01-08 15:54:56 +0000

    #2-filter-user-by-json

--- internal/models/user.go
+++ internal/models/user.go
@@ -22,11 +22,11 @@ var userFields = map[string][]string{
 
 type User struct {
 	ID        int       `json:"id" db:"id" binding:"required"`
-	Username  string    `json:"username"   form:"username" valid:"MaxSize(50)" db:"user_name"`
+	Username  string    `json:"username"   valid:"MaxSize(50)" db:"user_name"`
 	Password  string    `json:"password"   db:"password_hash"` // valid:"MaxSize(50)"
-	State     int8      `json:"state"      form:"state" valid:"Range(1, 5)"`
+	State     int8      `json:"state"      valid:"Range(1, 5)"`
 	CreatedAt time.Time `json:"created_at" db:"created_at"`
-	Email     string    `json:"email"      form:"email" valid:"Email"`
+	Email     string    `json:"email"      valid:"Email"`
 }
 
 // SetPassword sets a new password stored as hash.
