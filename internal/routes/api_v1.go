e0b4484d85b79ec58a7a31c0414d7967ef5a8f05
Maxim Hryshyn <goooglemax1993@gmail.com>
2021-02-16 12:13:57 +0300

    controllers-approach move auth to RegisterAPIV1

--- internal/routes/api_v1.go
+++ internal/routes/api_v1.go
@@ -8,13 +8,12 @@ import (
 
 // RegisterAPIV1 initialize routing information
 func RegisterAPIV1(router gin.IRouter, userController *controllers.UserController) {
-	router.Use(jwt.JWT())
-
 	users := router.Group("users")
-
-	users.GET("/", userController.GetUsers)
-	users.POST("/", userController.CreateUser)
-	users.GET("/:id", userController.GetUserByID)
-	users.PUT("/:id", userController.UpdateUser)
-	users.DELETE("/:id", userController.DeleteUser)
+	router.POST("/auth", userController.Authenticate)
+	router.POST("/create", userController.CreateUser)
+	users.GET("/", userController.GetUsers).Use(jwt.JWT())
+	users.POST("/", userController.CreateUser).Use(jwt.JWT())
+	users.GET("/:id", userController.GetUserByID).Use(jwt.JWT())
+	users.PUT("/:id", userController.UpdateUser).Use(jwt.JWT())
+	users.DELETE("/:id", userController.DeleteUser).Use(jwt.JWT())
 }
