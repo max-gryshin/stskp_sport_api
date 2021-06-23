60db38b0e35ae53e2728e70af9cabd19372250bd
maksim <m.hryshyn@andersenlab.com>
2021-06-23 14:59:39 +0300

    user repository

--- go.mod
+++ go.mod
@@ -21,5 +21,5 @@ require (
 	golang.org/x/net v0.0.0-20200904194848-62affa334b73 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
-	gopkg.in/yaml.v2 v2.3.0 // indirect
+	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 )
---
+++ internal/contractions/user_repotory.go
@@ -0,0 +1,13 @@
+package contractions
+
+import "github.com/ZmaximillianZ/stskp_sport_api/internal/models"
+
+// UserRepository is interface to communicate with user storage
+type UserRepository interface {
+	GetByID(id int) (models.User, error)
+	GetByUsername(username string) (models.User, error)
+	GetUsers() (models.Users, error)
+	CreateUser(user *models.User) error
+	UpdateUser(user *models.User) error
+	Delete(user *models.User) error
+}
